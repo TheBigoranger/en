caeb4ea431f409a9e13f47bb28b8f26d2db3b628
TheBigoranger <ethanxuyicheng@gmail.com>
2025-01-22 12:11:49 -0800

    Site updated: 2025-01-22 12:11:49

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,72 +1,50 @@
-$(function () {
-  $('.toggle-sidebar-info > span').on('click', function () {
-    var toggleText = $(this).attr('data-toggle')
-    $(this).attr('data-toggle', $(this).text())
-    $(this).text(toggleText)
-    changeSideBarInfo()
-  })
-  $('#toggle-sidebar').on('click', function () {
-    if (!isMobile() && $('#sidebar').is(':visible')) {
-      var isOpen = $(this).hasClass('on')
-      isOpen ? $(this).removeClass('on') : $(this).addClass('on')
-      if (isOpen) {
-        $('#page-header').removeClass('open-sidebar')
-        $('body').velocity('stop').velocity({
-          paddingLeft: '0px'
-        }, {
-          duration: 200
-        })
-        $('#sidebar').velocity('stop').velocity({
-          translateX: '0px'
-        }, {
-          duration: 200
-        })
-        $('#toggle-sidebar').velocity('stop').velocity({
-          rotateZ: '0deg',
-          color: '#1F2D3D'
-        }, {
-          duration: 200
-        })
-      } else {
-        $('#page-header').addClass('open-sidebar')
-        $('body').velocity('stop').velocity({
-          paddingLeft: '300px'
-        }, {
-          duration: 200
-        })
-        $('#sidebar').velocity('stop').velocity({
-          translateX: '300px'
-        }, {
-          duration: 200
-        })
-        $('#toggle-sidebar').velocity('stop').velocity({
-          rotateZ: '180deg',
-          color: '#99a9bf'
-        }, {
-          duration: 200
-        })
+/* global CONFIG */
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const isRight = CONFIG.sidebar.position === 'right';
+
+  const sidebarToggleMotion = {
+    mouse: {},
+    init() {
+      window.addEventListener('mousedown', this.mousedownHandler.bind(this));
+      window.addEventListener('mouseup', this.mouseupHandler.bind(this));
+      document.querySelector('.sidebar-dimmer').addEventListener('click', this.clickHandler.bind(this));
+      document.querySelector('.sidebar-toggle').addEventListener('click', this.clickHandler.bind(this));
+      window.addEventListener('sidebar:show', this.showSidebar);
+      window.addEventListener('sidebar:hide', this.hideSidebar);
+    },
+    mousedownHandler(event) {
+      this.mouse.X = event.pageX;
+      this.mouse.Y = event.pageY;
+    },
+    mouseupHandler(event) {
+      const deltaX = event.pageX - this.mouse.X;
+      const deltaY = event.pageY - this.mouse.Y;
+      const clickingBlankPart = Math.hypot(deltaX, deltaY) < 20 && event.target.matches('.main');
+      // Fancybox has z-index property, but medium-zoom does not, so the sidebar will overlay the zoomed image.
+      if (clickingBlankPart || event.target.matches('img.medium-zoom-image')) {
+        this.hideSidebar();
       }
+    },
+    clickHandler() {
+      document.body.classList.contains('sidebar-active') ? this.hideSidebar() : this.showSidebar();
+    },
+    showSidebar() {
+      document.body.classList.add('sidebar-active');
+      const animateAction = isRight ? 'fadeInRight' : 'fadeInLeft';
+      document.querySelectorAll('.sidebar .animated').forEach((element, index) => {
+        element.style.animationDelay = (100 * index) + 'ms';
+        element.classList.remove(animateAction);
+        setTimeout(() => {
+          // Trigger a DOM reflow
+          element.classList.add(animateAction);
+        });
+      });
+    },
+    hideSidebar() {
+      document.body.classList.remove('sidebar-active');
     }
-  })
-  function changeSideBarInfo () {
-    if ($('.author-info').is(':visible')) {
-      $('.author-info').velocity('stop')
-        .velocity('transition.slideLeftOut', {
-          duration: 300,
-          complete: function () {
-            $('.sidebar-toc').velocity('stop')
-              .velocity('transition.slideRightIn', { duration: 500 })
-          }
-        })
-    } else {
-      $('.sidebar-toc').velocity('stop')
-        .velocity('transition.slideRightOut', {
-          duration: 300,
-          complete: function () {
-            $('.author-info').velocity('stop')
-              .velocity('transition.slideLeftIn', { duration: 500 })
-          }
-        })
-    }
-  }
-})
+  };
+  sidebarToggleMotion.init();
+});
